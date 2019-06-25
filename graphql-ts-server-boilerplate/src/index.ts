066a15d03fe36d61679931e3f7c8623d6edc8565
CrisRod <crodriguez85@gmail.com>
2019-06-25 12:40:07 -0400

    import schema on index

--- graphql-ts-server-boilerplate/src/index.ts
+++ graphql-ts-server-boilerplate/src/index.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import { GraphQLServer } from 'graphql-yoga';
+import { importSchema } from 'graphql-import';
 
+const typeDefs = importSchema("schema.graphql")
 const resolvers = {
     Query: {
         hello: (_: any, { name }: any ) => `Hello ${name || 'world'}`,
     },
 }
 
-const server = new GraphQLServer({ resolvers })
+const server = new GraphQLServer({ typeDefs, resolvers })
 server.start(() => console.log('Server is running on localhost:4000'))
